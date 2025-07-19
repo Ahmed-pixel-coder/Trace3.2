0e5c67010db22ab7a28a106867e7145582e5a5af
Ahmed-pixel-coder <ahmed.m.f.a.224@gmail.com>
2025-07-19 22:43:41 +0200

    fix: properly initialize and export Firebase database

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,19 @@
-// src/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBMZOgZynZfM-9oWdSy745ISmeRZgkRqJw",
-    authDomain: "trace3-2.firebaseapp.com",
-    databaseURL: "https://trace3-2-default-rtdb.firebaseio.com",
-    projectId: "trace3-2",
-    storageBucket: "trace3-2.firebasestorage.app",
-    messagingSenderId: "871313519157",
-    appId: "1:871313519157:web:f64aa4824f75ae496c377c",
-    measurementId: "G-76Q7153TX7"
+  apiKey: "AIzaSyBMZOgZynZfM-9oWdSy745ISmeRZgkRqJw",
+  authDomain: "trace3-2.firebaseapp.com",
+  databaseURL: "https://trace3-2-default-rtdb.firebaseio.com",
+  projectId: "trace3-2",
+  storageBucket: "trace3-2.firebasestorage.app",
+  messagingSenderId: "871313519157",
+  appId: "1:871313519157:web:f64aa4824f75ae496c377c",
+  measurementId: "G-76Q7153TX7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
 export const db = getDatabase(app);
